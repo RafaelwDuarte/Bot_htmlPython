77c1212453906e91b5b421e43f27252b44d98145
RafaelwDuarte <47457329+RafaelwDuarte@users.noreply.github.com>
2019-06-03 15:38:59 -0300

    Update Bot_py_script_UNI.py

--- Bot_py_script_UNI.py
+++ Bot_py_script_UNI.py
@@ -2,44 +2,77 @@ import http.client, urllib.parse, requests
 from html.parser import HTMLParser
 import rstr
 import random
+import copy
+import datetime
 
+#Inicialização de variaveis 
 Action = str()
+dados_form_con = list()
 dados_form = list()
+
+#Conta Requisições
+numero_vezes = int(input("Numero de Requisições: "))
+cont = 0 
+#Seta data 
+today  = datetime.datetime.today()
+today = today.strftime("%Y-%m-%d %H:%M")
+
+#Loga Inicio
+arquivo = open('log.log','a')
+arquivo.write("\n" + "\n" + "---> Inicio do Script  " + today + " -  Numero de repetições:  " + str(numero_vezes))
+arquivo.close()
 
 #Requisição HTTP
 r = requests.get('http://www.agexcom.com.br/hackform/index.php')
-#Codigo de resposta
-print(r.status_code)
+http_code = r.status_code
 
-#Pesquisar no parser e monta um dicionario com os parametros
+#Pesquisar no parser e monta uma lista com os parametros
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if "input" in tag:
             Action=dict(attrs)["name"]
-            dados_form.append(Action)
+            dados_form_con.append(Action)
         if "select" in tag:
             Action=dict(attrs)["name"]
-            dados_form.append(Action)
-#monta parser
+            dados_form_con.append(Action)
+#Parser no HTML
 parser = MyHTMLParser()
 parser.feed(r.text)
 
-#print dicionario
-print(dados_form)
+#Remove Dados
+dados_form_con.remove("submit")
+dados_form_con.remove("gender")
 
-#remove botões:
-dados_form.remove("submit")
-dados_form.remove("gender")
+while cont < numero_vezes: 
 
-#Seta valores randomicos para o dicionario dados_form
-dados_form[0] = '{0}{1}{2}{3}{4}{5}{6}'.format(rstr.uppercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1))
-dados_form[1] = '{0}{1}'.format(rstr.uppercase(1), rstr.lowercase())
-gender = ['Masculino','Feminino']
-dados_form[2] = random.choice(gender)
-dados_form[3] = '{0}{1}/{2}{3}/{4}{5}{6}'.format(rstr.rstr('012', 1), rstr.digits(1), rstr.rstr('01', 1), rstr.rstr('012', 1), rstr.rstr('12', 1), rstr.rstr('09', 2), rstr.digits(1))
-dados_form[4] = '{0}@{1}.com.{2}'.format(rstr.uppercase(exclude='@#<"^;:´`?+=[]~*{}'), rstr.lowercase(5,15), rstr.lowercase(2))
-dados_form[5] = '{0}{1}'.format(rstr.rstr('3,8,9', 1), rstr.digits(7))
-sist = ['Debian', 'Ubuntu', 'Windows', 'Mac OS']
-dados_form[6] = random.choice(sist)
+    #Copia de Lista
+    dados_form = copy.copy(dados_form_con)
 
-print(dados_form)
+    #Popula lista
+    dados_form[0] = '{0}{1}{2}{3}{4}{5}{6}'.format(rstr.uppercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1))
+    dados_form[1] = '{0}{1}'.format(rstr.uppercase(1), rstr.lowercase(5,16))
+    gender = ['Masculino','Feminino']
+    dados_form[2] = random.choice(gender)
+    dados_form[3] = '{0}{1}/{2}{3}/{4}{5}{6}'.format(rstr.rstr('012', 1), rstr.digits(1), rstr.rstr('01', 1), rstr.rstr('012', 1), rstr.rstr('12', 1), rstr.rstr('09', 2), rstr.digits(1))
+    dados_form[4] = '{0}@{1}.com.{2}'.format(rstr.uppercase(exclude='@#<"^;:´`?+=[]~*{}'), rstr.lowercase(5,15), rstr.lowercase(2))
+    dados_form[5] = '{0}{1}'.format(rstr.rstr('3,8,9', 1), rstr.digits(7))
+    sist = ['Debian', 'Ubuntu', 'Windows', 'Mac OS']
+    dados_form[6] = random.choice(sist)
+
+    #Log
+    arquivo = open('log.log','a')
+    arquivo.write("\n" + "\n" + "-------Forumlario  " + str(cont+1) + "\n" + " Http code: " + str(http_code) + "\n" + " Campos:")
+    for i in dados_form_con:
+        arquivo.writelines(i)
+        arquivo.write(',')
+    arquivo.close()
+    arquivo = open('log.log','a')
+    arquivo.write("\n " + "Dados: ")
+    for i in dados_form:
+        arquivo.write(i)
+        arquivo.write(',')
+    arquivo.close()
+    cont = cont + 1
+print ("Concluído com Sucesso")
+
+
