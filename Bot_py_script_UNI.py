1608720d0f8ae0d6e748ad4246650fe533409eeb
RafaelwDuarte <47457329+RafaelwDuarte@users.noreply.github.com>
2019-06-21 15:43:06 -0300

    Update Bot_py_script_UNI.py

--- Bot_py_script_UNI.py
+++ Bot_py_script_UNI.py
@@ -1,5 +1,6 @@
 import http.client, urllib.parse, requests, json, rstr, random, datetime, copy
 from html.parser import HTMLParser
+import urllib
 
 #Inicialização de variaveis 
 Action = str()
@@ -50,9 +51,9 @@ sumary = copy.copy(dados_form_con)
 
 #Postagem do formulario 
 def payload(site, act, cont, numero_vezes):
-    payload={dados_form_con[0]:dados_form[0],dados_form_con[1]:dados_form[1],dados_form_con[2]:dados_form[2],dados_form_con[3]:dados_form[3],dados_form_con[4]:dados_form[4],dados_form_con[5]:dados_form[5],dados_form_con[6]:dados_form[6]}
+    payload = dict(zip(dados_form_con,dados_form))
     site = site + "/" + act[0] 
-    enviar = requests.post(site, payload)
+    enviar = requests.post(site , payload)
     envioresp = enviar.status_code
     Progreso = cont
     if (envioresp == 200):
@@ -61,13 +62,6 @@ def payload(site, act, cont, numero_vezes):
         print ("Requisição" , Progreso , "/" , numero_vezes, " - Erro") 
     #print("Envio", enviar.reason)
 
-#Juntando as listas 
-def together_list():
-    cont1 = 0
-    while cont1 < len(dados_form):
-        dados.append(dados_form_con[cont1]+":")
-        dados.append(dados_form[cont1]+",")
-        cont1 = cont1 + 1
 #Log 
 def log(cont, dados_form):
     arquivo = open('log.log','a')
@@ -87,27 +81,34 @@ def Popula(numero_vezes, Progreso):
             print ("Campo: ", dados_form_con)
             mode = str(input("Escolha a letra do modo de inserção de dados" + "\n" "[R]random - Campos fixos" + "\n" + "[E]arquivo externo" + "\n" + "[C]Passar dados Manualmente \n"))
             if (mode == "R" or mode == "r"):
-                mode2 = str(input("(N)nome \n (SN)Sobrenome \n (G)Genero \n (DN)DataDeNascimento \n (E)Email \n (T)Telefone \n (SO)SistemaOperacional \n"))
+                mode2 = str(input(" (N)nome \n (SN)Sobrenome \n (G)Genero \n (DN)DataDeNascimento \n (E)Email \n (T)Telefone \n (SO)SistemaOperacional \n"))
                 if (mode2 == "N"):
                     dados_form[conti] = '{0}{1}{2}{3}{4}{5}{6}'.format(rstr.uppercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1), rstr.rstr('aeiou', 1), rstr.lowercase(1))
+                    conti = conti + 1
                 elif (mode2 == "SN"):
                     dados_form[conti] = '{0}{1}'.format(rstr.uppercase(1), rstr.lowercase(5,16))
+                    conti = conti + 1
                 elif (mode2 == "G"):
                     gender = ['male','female']
                     dados_form[conti] = random.choice(gender)
+                    conti = conti + 1
                 elif (mode2 == "DN"):
                     ano_inic = ["198","197","196","195","194","193","192","199","200","201"]
                     ano_random = random.choice(ano_inic) + rstr.digits(1)
                     data_rand_nasc = '{0}{1}/{2}{3}/'.format(rstr.rstr('012', 1), rstr.digits(1), rstr.rstr('01', 1), rstr.rstr('012', 1))
                     data_rand_nasc = data_rand_nasc + ano_random
                     dados_form[conti] = data_rand_nasc
+                    conti = conti + 1
                 elif (mode2 == "E"):
                     dados_form[conti] = '{0}@{1}.com.{2}'.format(rstr.lowercase(exclude='@#<"^;:´`?+=[]~*{}'), rstr.lowercase(5,15), rstr.lowercase(2))
+                    conti = conti + 1
                 elif (mode2 == "T"):
                     dados_form[conti] = '{0}{1}'.format(rstr.rstr('3,8,9', 1), rstr.digits(7))
+                    conti = conti + 1
                 elif (mode2 == "SO"):
                     sist = ['Debian', 'Ubuntu', 'Windows', 'Mac OS']
                     dados_form[conti] = random.choice(sist)
+                    conti = conti + 1
                 else:
                     print ("Opção invalida")
             elif (mode == "E" or mode == "e"):
@@ -115,12 +116,12 @@ def Popula(numero_vezes, Progreso):
                     try:
                         with open(arqex) as ArquivoExt:
                             dados_form[conti] = ArquivoExt.readline().rstrip('\n')
+                            conti = conti + 1
                     except:
                         print("Arquivo ou caminho com erro ou fora do padrão")
             elif (mode == "C" or mode == "c"):
-                dados_form[conti] = input("")
-            together_list()
-            conti = conti + 1
+                dados_form[conti] = input("Insira constante: ")
+                conti = conti + 1
         cont = cont + 1
         payload(site, act, cont, numero_vezes)
         log(cont , dados_form)
